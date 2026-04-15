5d0042c1518e6fc9d662836351e29c05c11e88b7
Max Rossmannek <oss@zurich.ibm.com>
2026-04-15 11:50:33 +0200

    fix: also specify the search-path

--- crates/pyext/build.rs
+++ crates/pyext/build.rs
@@ -27,8 +27,9 @@ fn main() {
     }
 
     let qiskit_lib_dir = qiskit_lib_path.parent().unwrap().to_str().unwrap();
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", qiskit_lib_dir);
-
     let qiskit_lib_name = qiskit_lib_path.file_name().unwrap().to_str().unwrap();
+
+    println!("cargo:rustc-link-search={}", qiskit_lib_dir);
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", qiskit_lib_dir);
     println!("cargo:rustc-link-lib=dylib:+verbatim={}", qiskit_lib_name);
 }
