299596bc8cc8d7c2198b5c254ac731724b0e2440
Max Rossmannek <oss@zurich.ibm.com>
2026-04-17 09:55:03 +0200

    [wip] ci: attempt to work around Python.h not being found on Windows CI

--- crates/qiskit-sys/build.rs
+++ crates/qiskit-sys/build.rs
@@ -61,6 +61,7 @@ fn generate_bindings_c() {
 fn generate_bindings_py() {
     let qiskit_lib = env::var("QISKIT_LIB").unwrap();
     let qiskit_include = env::var("QISKIT_INCLUDE").unwrap();
+    let python_include = env::var("PYTHON_INCLUDE").unwrap();
 
     let qiskit_lib_path = Path::new(&qiskit_lib);
 
@@ -79,6 +80,7 @@ fn generate_bindings_py() {
     println!("cargo:rustc-link-arg={}", qiskit_lib);
 
     let bindings: bindgen::Bindings = bindgen::Builder::default()
+        .clang_arg(format!("-I{}", python_include))
         .clang_arg(format!("-I{}", qiskit_include))
         .clang_arg("-DQISKIT_C_PYTHON_INTERFACE=1")
         .raw_line("use pyo3::ffi::PyObject;")
