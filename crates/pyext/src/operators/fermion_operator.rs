afc9737b5d6dfc1c928430eb29666faa1475b36e
Max Rossmannek <21973473+mrossinek@users.noreply.github.com>
2026-06-17 10:09:29 +0200

    Rename `many_body_order` to `max_rank` (#142)
    * refactor: rename many_body_order method to max_rank
    
    * refactor: improve core implementation of max_rank
    
    * fix: typos in C API examples

--- crates/pyext/src/operators/fermion_operator.rs
+++ crates/pyext/src/operators/fermion_operator.rs
@@ -326,7 +326,7 @@ impl FermionOperatorDataIter {
 /// .. autosummary::
 ///
 ///    is_hermitian
-///    many_body_order
+///    max_rank
 ///    conserves_particle_number
 ///
 /// ----
@@ -965,11 +965,12 @@ impl PyFermionOperator {
         self.inner.is_hermitian(atol)
     }
 
-    /// Returns the many-body order of this operator.
+    /// Returns the maximum rank of the terms in this operator.
     ///
     /// .. note::
-    ///    The many-body order is defined as the length of the longest term contained in the
-    ///    operator.
+    ///    The length of the longest term can depend on the operator's form which means that (for
+    ///    example) operator simplification or normal-ordering can result in a different maximum
+    ///    rank.
     ///
     /// .. doctest::
     ///
@@ -977,13 +978,13 @@ impl PyFermionOperator {
     ///     >>> op = FermionOperator.from_dict({
     ///     ...     ((True, 0), (False, 1), (True, 2), (False, 3)): 1,
     ///     ... })
-    ///     >>> op.many_body_order()
+    ///     >>> op.max_rank()
     ///     4
     ///
     /// Returns:
-    ///     The many-body order of this operator.
-    fn many_body_order(&self) -> u32 {
-        self.inner.many_body_order()
+    ///     The maximum rank of this operator.
+    fn max_rank(&self) -> u32 {
+        self.inner.max_rank()
     }
 
     /// Returns whether this operator is particle-number conserving.
--- tests/python/operators/test_majorana_operator.py
+++ tests/python/operators/test_majorana_operator.py
@@ -301,23 +301,28 @@ class TestMajoranaOperator:
         assert not op.is_hermitian()
         assert op.is_hermitian(1e-4)
 
-    def test_many_body_order(self, subtests):
+    def test_max_rank(self, subtests):
         cls = self.get_class()
 
-        op = cls.one()
+        op = cls.zero()
 
-        with subtests.test("0"):
-            assert op.many_body_order() == 0
+        with subtests.test("0 for additive identity"):
+            assert op.max_rank() == 0
+
+        op += cls.one()
+
+        with subtests.test("0 for multiplicative identity"):
+            assert op.max_rank() == 0
 
         op += cls.from_dict({(0, 1): 1})
 
         with subtests.test("2"):
-            assert op.many_body_order() == 2
+            assert op.max_rank() == 2
 
         op += cls.from_dict({(0, 1, 2, 3): 1})
 
         with subtests.test("4"):
-            assert op.many_body_order() == 4
+            assert op.max_rank() == 4
 
     def test_is_even(self, subtests):
         cls = self.get_class()
