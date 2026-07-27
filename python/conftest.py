9ce445d798a3d062dd6a572393c25278109bb806
Max Rossmannek <oss@zurich.ibm.com>
2026-07-27 14:50:17 +0200

    ci: also lint python/*.py

--- python/conftest.py
+++ python/conftest.py
@@ -10,6 +10,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+"""Configuration of Sybil doctests integration for pytest."""
+
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 
 from sybil import Sybil
